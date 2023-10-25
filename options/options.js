755ec8fa8b32cf457df9377d719f3835b7a92832
Crowdbotics <team@crowdbotics.com>
2023-10-25 15:49:52 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tri-state-removals-44145",
+  url: "https://tri-state-removals-44145.botics.co",
+  api: "https://tri-state-removals-44145.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
